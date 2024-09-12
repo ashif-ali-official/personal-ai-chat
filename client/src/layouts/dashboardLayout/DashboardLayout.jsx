8e999bb369d55a5bc3e6563609811b770b289775
Ashif Ali <ashif.mac@Ashifs-MacBook-Air.local>
2024-09-12 14:20:34 +0530

    ImageKitIO & Upload done

--- client/src/layouts/dashboardLayout/DashboardLayout.jsx
+++ client/src/layouts/dashboardLayout/DashboardLayout.jsx
@@ -2,7 +2,7 @@ import { Outlet, useNavigate } from 'react-router-dom'
 import './DashboardLayout.css'
 import { useAuth } from '@clerk/clerk-react'
 import { useEffect } from 'react'
-import ChatList from './../../components/ChatList';
+import ChatList from './../../components/chatList/ChatList';
 
 function DashboardLayout() {
 
